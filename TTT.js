a242a37975fadf22fb28bcf679180e370da208d2
drejmin <djjamn@gmail.com>
2023-06-30 17:09:07 -0700

    modified:   TTT.css 	modified:   TTT.js 	modified:   TTT.html

--- TTT.js
+++ TTT.js
@@ -14,7 +14,6 @@ let winner; // null= no winner; 1 or -1 winner;  T = cat game;
 const message =document.querySelector('h1');
 const playAgainBtn = document.querySelector('button');
 
-
 //-------------------------event listeners---------------------------
 playAgainBtn.addEventListener('click',init);
 document.getElementById('board').addEventListener('click',placeMarker);
@@ -28,8 +27,8 @@ board =
     null,null,null,
     null,null,null]
 
-turn =1;
-winner =null;
+turn = 1;
+winner = null;
 render();
 }
 
@@ -41,14 +40,10 @@ function placeMarker(evt) {
      return;
 
     board[Idx]=turn;
-    turn *=-1;
+    console.log(board);
     winner = getWinner();
+    turn *=-1;
     render();
-}
-{
-turn = 1;
-winner = null;
-render();
 }
 
 function render() {
@@ -56,20 +51,31 @@ function render() {
   renderMessage();
   renderControls();
   playAgainBtn.disabled =!winner;
+}
 
 function getWinner(){
-    
-  // Visualize all state in the DOM
-  }
-  render();
-  function renderBoard() {
-    board.forEach(function (bxVal, Idx) {
-      const boxEl = document.getElementById(`bx-${Idx}`);
+    // if/else conditional for any way a current player can win
+    // 012, 345, 678, 036, 147, 258, 048,246
+    if (-1===board[0,1,2]|| 1===board[0,1,2]){return renderMessage();}
+    else if (-1===board[3,4,5]|| 1===board[3,4,5]){return renderMessage();}
+    else if (-1===board[6,7,8]|| 1===board[6,7,8]){return renderMessage();}
+    else if (-1===board[0,3,6]|| 1===board[0,3,6]){return renderMessage();}
+    else if (-1===board[1,4,7]|| 1===board[1,4,7]){return renderMessage();}
+    else if (-1===board[2,5,8]|| 1===board[2,5,8]){return renderMessage();}
+    else if (-1===board[0,4,8]|| 1===board[0,4,8]){return renderMessage();}
+    else if (-1===board[2,4,6]|| 1===board[2,4,6]){return renderMessage();}
+    else if (!board.includes('null')){
+        return winner === 'T'};
+}
+
+function renderBoard() {
+    board.forEach((bxVal, Idx) => {
+        const boxEl = document.getElementById(`bx-${Idx}`);
         boxEl.style.backgroundColor = PLAYERS[bxVal];
-      });
-    };
+    });
+};
   
-  function renderMessage() {
+function renderMessage() {
     if (winner === 'T') {
       message.innerText = "It's a Cat Game!!!";
     } else if (winner) {
@@ -78,15 +84,15 @@ function getWinner(){
       // Game is in play
       message.innerHTML = `<span style="color: ${PLAYERS[turn]}">${PLAYERS[turn].toUpperCase()}</span>'s Turn`;
     }
-  }
+}
   
-  function renderControls() {
+function renderControls() {
     
     playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
    
-    board.forEach(function(bxVal, colIdx) {
-      const hideMarker = !board[colIdx].includes(0) || winner;
-      playAgainBtn.style.visibility = hideMarker ? 'hidden' : 'visible';
+    board.forEach(function(Idx) {
+       const hideButton = !board[Idx].includes('null') || winner;
+      playAgainBtn.style.visibility = hideButton ? 'hidden' : 'visible';
     });
-  }
-};
+ }
+
